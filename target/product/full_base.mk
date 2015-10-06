fd3fa2740f7a6e3a278a0375371bc5ee5a091c09
rvkasper <rvkasper@gmail.com>
2015-10-06 16:36:50 -0600

    Default sounds update

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -40,7 +40,7 @@ PRODUCT_PACKAGES += \
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+    ro.config.notification_sound=Tethys.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
