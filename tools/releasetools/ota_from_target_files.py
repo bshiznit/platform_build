7d7e3269ace89de03af603ead34ddf5302ebb964
rvkasper <rvkasper@gmail.com>
2015-11-05 11:22:52 -0700

    Bump to MRA58N

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -573,7 +573,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: %s" % CalculateFingerprint(
       oem_props, oem_dict, OPTIONS.info_dict))
 
-  script.Print("MelloShmallow-MRA58K")
+  script.Print("MelloShmallow-MRA58N")
   device_specific.FullOTA_InstallBegin()
 
   if block_based:
