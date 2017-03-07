8776d594787fe4b7db53367c0bbb84e133cd929c
Ryan Kasper <rvkasper@gmail.com>
2017-03-07 02:58:05 -0700

    N4F26T

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -618,7 +618,7 @@ reboot_now("%(bcb_dev)s", "recovery");
 else if get_stage("%(bcb_dev)s") == "3/3" then
 """ % bcb_dev)
 
-  script.Print("Noougies-N4F26Q")
+  script.Print("Noougies-N4F26T")
   # Dump fingerprints
   script.Print("Target: %s" % CalculateFingerprint(
       oem_props, oem_dict, OPTIONS.info_dict))
